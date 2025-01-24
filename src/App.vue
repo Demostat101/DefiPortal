2094930178207f69a90bbb1ed0241e9fd0dca90b
Demostat101 <demostat1995@gmail.com>
2025-01-24 15:50:48 +0100

    done

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,14 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
 import { RouterView } from 'vue-router'
 import TopBar from './views/TopBar.vue'
 import FooterPage from './views/FooterPage.vue'
-
-interface Wallet {
-  CoinName: string
-  Symbol: string
-  ImageUrl: string
-  Id: string
-}
-
-const wallets = ref<Wallet[]>([])
-
-onMounted(async () => {
-  try {
-    const response = await fetch('https://min-api.cryptocompare.com/data/all/coinlist')
-    const data = await response.json()
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    wallets.value = Object.values(data.Data).map((walletData: any) => ({
-      CoinName: walletData?.CoinName ?? '',
-      Symbol: walletData?.Symbol ?? '',
-      ImageUrl: `https://www.cryptocompare.com${walletData?.ImageUrl ?? ''}`,
-      Id: walletData?.Id ?? '',
-    }))
-  } catch (error) {
-    console.error('Error fetching wallet data:', error)
-  }
-})
 </script>
 
 <template>
   <div class="flex flex-col h-screen bg-black relative overflow-y-auto hide-scrollbar">
     <TopBar />
     <main class="p-8 grow">
-      <RouterView :wallets="wallets" />
+      <RouterView />
     </main>
     <FooterPage />
   </div>
--- src/views/ConnectPage.vue
+++ src/views/ConnectPage.vue
@@ -1,29 +1,361 @@
 <script setup lang="ts">
-import { onMounted, ref, watch, onBeforeUnmount, nextTick } from 'vue'
-import { defineProps } from 'vue'
+import { onMounted, ref, onBeforeUnmount, nextTick } from 'vue'
+
+const walletData = [
+  {
+    image: '/images/wallet1.png',
+    name: 'Trustee wallet',
+  },
+  {
+    image: '/images/wallet2.jpg',
+    name: 'Zano wallet',
+  },
+  {
+    image: '/images/wallet3.png',
+    name: 'Klever wallet',
+  },
+  {
+    image: '/images/wallet4.jpg',
+    name: 'Defichain wallet',
+  },
+  {
+    image: '/images/wallet6.jpg',
+    name: 'Dfx wallet',
+  },
+  {
+    image: '/images/wallet7.png',
+    name: 'Pontem wallet',
+  },
+  {
+    image: '/images/wallet8.png',
+    name: 'Aptos wallet',
+  },
+  {
+    image: '/images/wallet9.jpg',
+    name: 'Vite wallet',
+  },
+  {
+    image: '/images/wooden-f-1346174_640.png',
+    name: 'Flare wallet',
+  },
+  {
+    image: '/images/wallet11.png',
+    name: 'Hydra wallet',
+  },
+  {
+    image: '/images/wallet12.png',
+    name: 'Auro wallet',
+  },
+  {
+    image: '/images/wallet13.jpg',
+    name: 'Sub wallet',
+  },
+  {
+    image: '/images/wallet14.png',
+    name: 'Clover wallet',
+  },
+  {
+    image: '/images/wallet15.jpg',
+    name: 'Blixt wallet',
+  },
+  {
+    image: '/images/wallet16.jpg',
+    name: 'Sparrow wallet',
+  },
+  {
+    image: '/images/wallet17.png',
+    name: 'BlackFort wallet',
+  },
+  {
+    image: '/images/wallet18.png',
+    name: 'MetaMask wallet',
+  },
+  {
+    image: '/images/wallet19.jpg',
+    name: 'Trust wallet',
+  },
+  {
+    image: '/images/wallet20.png',
+    name: 'Coinbase wallet',
+  },
+  {
+    image: '/images/wallet21.webp',
+    name: 'Exodus',
+  },
+  {
+    image: '/images/wallet22.png',
+    name: 'Mycelium',
+  },
+  {
+    image: '/images/wallet23.png',
+    name: 'Ledger Nano S',
+  },
+  {
+    image: '/images/wallet23.png',
+    name: 'Ledger Nano X',
+  },
+  {
+    image: '/images/wallet25.png',
+    name: 'Trezor One',
+  },
+  {
+    image: '/images/wallet25.png',
+    name: 'Trezor Model T',
+  },
+  {
+    image: '/images/wallet27.svg',
+    name: 'Atomic wallet',
+  },
+  {
+    image: '/images/wallet28.jpg',
+    name: 'BRD wallet',
+  },
+  {
+    image: '/images/wallet29.jpg',
+    name: 'Jaxx Liberty',
+  },
+  {
+    image: '/images/wallet30.jpg',
+    name: 'Edge wallet',
+  },
+  {
+    image: '/images/wallet31.png',
+    name: 'Samourai wallet',
+  },
+  {
+    image: '/images/wallet32.png',
+    name: 'Wasabi wallet',
+  },
+  {
+    image: '/images/wallet33.jpg',
+    name: 'Guarda wallet',
+  },
+  {
+    image: '/images/wallet34.png',
+    name: 'Infinto wallet',
+  },
+  {
+    image: '/images/wallet35.png',
+    name: 'Zengo',
+  },
+  {
+    image: '/images/wallet36.png',
+    name: 'Ellipal',
+  },
+  {
+    image: '/images/wallet37.png',
+    name: 'KeepKey',
+  },
+  {
+    image: '/images/wallet38.png',
+    name: 'Bitcoin.com wallet',
+  },
+  {
+    image: '/images/wallet39.png',
+    name: 'Coins.ph',
+  },
+  {
+    image: '/images/wallet40.png',
+    name: 'Coinomi',
+  },
+  {
+    image: '/images/wallet41.png',
+    name: 'Blockchain wallet',
+  },
+  {
+    image: '/images/wallet42.jpg',
+    name: 'Bitpay wallet',
+  },
+  {
+    image: '/images/wallet43.png',
+    name: 'Green wallet',
+  },
+  {
+    image: '/images/wallet44.png',
+    name: 'Copay',
+  },
+  {
+    image: '/images/wallet45.png',
+    name: 'Amory',
+  },
+  {
+    image: '/images/wallet46.png',
+    name: 'Electrum',
+  },
+  {
+    image: '/images/wallet47.png',
+    name: 'Monero GUI wallet',
+  },
+  {
+    image: '/images/wallet48.png',
+    name: 'BitBox',
+  },
+  {
+    image: '/images/wooden-n-1346197_640.png',
+    name: 'Narkom wallet',
+  },
+  {
+    image: '/images/wallet50.png',
+    name: 'Eidoo wallet',
+  },
+  {
+    image: '/images/wallet51.png',
+    name: 'Horizon wallet',
+  },
+  {
+    image: '/images/wallet52.png',
+    name: 'Pillar wallet',
+  },
+  {
+    image: '/images/wallet53.jpg',
+    name: 'Kinesis wallet',
+  },
+  {
+    image: '/images/wallet54.png',
+    name: ' SatoWallet',
+  },
+  {
+    image: '/images/wallet55.png',
+    name: 'Safepal',
+  },
+  {
+    image: '/images/wallet56.png',
+    name: 'Token Pocket',
+  },
+  {
+    image: '/images/wallet57.png',
+    name: 'Nova wallet',
+  },
+  {
+    image: '/images/wallet58.png',
+    name: 'Wallet Connect',
+  },
+  {
+    image: '/images/wallet59.png',
+    name: 'OKX',
+  },
+  {
+    image: '/images/wallet60.png',
+    name: 'Binance',
+  },
+  {
+    image: '/images/wallet62.png',
+    name: 'HyperPay wallet',
+  },
+  {
+    image: '/images/wallet63.png',
+    name: 'Tronlink',
+  },
+  {
+    image: '/images/wallet64.png',
+    name: 'Clore wallet',
+  },
+  {
+    image: '/images/wallet65.png',
+    name: 'Klever wallet',
+  },
+  {
+    image: '/images/wallet66.png',
+    name: 'Coin98',
+  },
+  {
+    image: '/images/wallet67.png',
+    name: 'Kardianchain wallet',
+  },
+  {
+    image: '/images/wallet68.png',
+    name: 'Blue wallet',
+  },
+  {
+    image: '/images/wallet69.png',
+    name: 'Satoshi wallet',
+  },
+  {
+    image: '/images/wallet70.png',
+    name: 'Phoenix wallet',
+  },
+  {
+    image: '/images/wallet71.png',
+    name: 'Bridge wallet',
+  },
+  {
+    image: '/images/wallet72.png',
+    name: 'Sora wallet',
+  },
+  {
+    image: '/images/wallet73.png',
+    name: 'Bravos wallet',
+  },
+  {
+    image: '/images/wallet74.png',
+    name: 'Leap wallet',
+  },
+  {
+    image: '/images/wallet75.jpg',
+    name: 'Web wallet',
+  },
+  {
+    image: '/images/wallet76.png',
+    name: 'Via wallet',
+  },
+  {
+    image: '/images/wallet77.png',
+    name: 'Auro wallet',
+  },
+  {
+    image: '/images/wallet78.png',
+    name: 'Bitfrost wallet',
+  },
+  {
+    image: '/images/wallet79.png',
+    name: 'Defichain wallet',
+  },
+  {
+    image: '/images/wallet80.png',
+    name: 'Cosmostation wallet',
+  },
+  {
+    image: '/images/wallet81.png',
+    name: 'Plena wallet',
+  },
+  {
+    image: '/images/wallet82.png',
+    name: 'Walken wallet',
+  },
+  {
+    image: '/images/wallet83.png',
+    name: 'Maths wallet',
+  },
+  {
+    image: '/images/wallet84.png',
+    name: 'Keplr wallet',
+  },
+  {
+    image: '/images/wallet86.png',
+    name: 'Helium wallet',
+  },
+  {
+    image: '/images/wallet85.png',
+    name: 'Fearless wallet',
+  },
+  {
+    image: '/images/wallet87.png',
+    name: 'CWallet',
+  },
+  {
+    image: '/images/wallet88.jpg',
+    name: 'Haqq wallet',
+  },
+]
 
 const isOpen = ref(false)
 const switchPage = ref('page1')
 
-// Define types for wallet data structure
-interface Wallet {
-  CoinName: string
-  Symbol: string
-  ImageUrl: string
-  Id: string
-}
-
-const props = defineProps({
-  wallets: {
-    type: Array as () => Wallet[],
-    required: true,
-  },
-})
-
-const selectWallet = (selectedWallet: { CoinName: string; ImageUrl: string }) => {
-  wallet.value = selectedWallet.CoinName
-  walletImage.value = selectedWallet.ImageUrl
+const selectWallet = (selectedWallet: { name: string; image: string }) => {
+  wallet.value = selectedWallet.name
+  walletImage.value = selectedWallet.image
   isOpen.value = false
+  getSelected.value = selectedWallet.name
 }
 
 // Declare refs with specific types
@@ -36,37 +368,13 @@ const email = ref<string>('') // Email as a string
 const walletFocus = ref<boolean>(false) // Focus state for wallet select
 const wallet = ref<string>('') // Selected wallet symbol as a string
 const walletImage = ref('')
+const getSelected = ref('')
 
 // Ref to store list of wallets
 const activeIndex = ref<number>(0) // Keeps track of the active index in the wallet list
 
 // Ref to store the wallet list container for scrolling
 const containerRef = ref<HTMLElement | null>(null)
-
-// Watch for changes in wallets and log whenever it updates
-watch(props.wallets, (newWallets) => {
-  console.log('Updated wallets:', newWallets)
-})
-
-const loading = ref(false) // Loading state for fetching wallets
-const currentPage = ref(1) // Current page number for pagination
-const pageSize = ref(20) // Number of wallets per page
-const slicedWallets = ref<Wallet[]>([])
-
-const fetchWallets = async () => {
-  if (loading.value) return // Prevent multiple simultaneous fetches
-  loading.value = true
-
-  // Simulate API call (replace this with your actual API call)
-  const newWallets = props.wallets.slice(
-    (currentPage.value - 1) * pageSize.value,
-    currentPage.value * pageSize.value,
-  )
-
-  slicedWallets.value.push(...newWallets) // Add new wallets to the existing list
-  currentPage.value++ // Increment page number for next fetch
-  loading.value = false
-}
 
 const toggleDropdown = () => {
   // Toggle focus and dropdown state
@@ -77,24 +385,13 @@ const toggleDropdown = () => {
   if (isOpen.value) {
     // Reset active index when the dropdown is opened
     activeIndex.value = 0
-    // Fetch the first batch of wallets
-    fetchWallets()
+
     nextTick(() => {
-      if (slicedWallets.value.length > 0) {
+      if (walletData.length > 0) {
         const firstItem = document.querySelector('.wallet-item') as HTMLElement
         firstItem?.focus()
       }
     })
-  }
-}
-
-const onScroll = () => {
-  const container = containerRef.value
-  if (!container) return
-
-  // Check if we've scrolled to the bottom
-  if (container.scrollTop + container.clientHeight >= container.scrollHeight - 10) {
-    fetchWallets() // Fetch more wallets if we're at the bottom
   }
 }
 
@@ -109,9 +406,8 @@ const handleClickOutside = (event: MouseEvent) => {
 // Handle keyboard navigation (ArrowUp, ArrowDown, Enter)
 const handleKeydown = (event: KeyboardEvent) => {
   if (isOpen.value) {
-    fetchWallets()
     if (event.key === 'ArrowDown') {
-      if (activeIndex.value < slicedWallets.value.length - 1) {
+      if (activeIndex.value < walletData.length - 1) {
         activeIndex.value++
       }
     } else if (event.key === 'ArrowUp') {
@@ -119,7 +415,7 @@ const handleKeydown = (event: KeyboardEvent) => {
         activeIndex.value--
       }
     } else if (event.key === 'Enter' && activeIndex.value >= 0) {
-      selectWallet(slicedWallets.value[activeIndex.value])
+      selectWallet(walletData[activeIndex.value])
       walletFocus.value = false
     }
   }
@@ -134,7 +430,6 @@ onMounted(() => {
 onBeforeUnmount(() => {
   document.removeEventListener('keydown', handleKeydown)
   document.removeEventListener('click', handleClickOutside)
-  fetchWallets()
 })
 </script>
 
@@ -182,7 +477,7 @@ onBeforeUnmount(() => {
             <div
               class="w-full flex flex-col gap-1 text-left border-r-0 border-l-0 transition duration-700 border-t-0 h-14 border-b-[2px] border-b-white"
             >
-              <label class="text-[#454a4d] text-sm" for="wallet">Select Wallet</label>
+              <label class="text-[#454a4d] text-sm" for="wallet"> Select Wallet </label>
 
               <div class="w-full relative">
                 <!-- Dropdown display (trigger) -->
@@ -196,7 +491,7 @@ onBeforeUnmount(() => {
                       v-if="walletImage"
                       :src="walletImage"
                       alt="Selected wallet"
-                      class="w-7 h-7 mr-2 rounded-full object-cover"
+                      class="w-7 h-7 mr-2 rounded-full object-cover bg-slate-300"
                     />
                     <span>{{ wallet ? wallet : 'Select a wallet' }}</span>
                   </span>
@@ -206,28 +501,35 @@ onBeforeUnmount(() => {
                 <div
                   @click="walletFocus = false"
                   v-show="isOpen"
-                  @scroll="onScroll"
                   ref="containerRef"
                   class="absolute data-[top-scroll=true] z-50 bottom-10 left-0 w-full bg-white shadow-lg rounded-md mb-1 max-h-64 overflow-y-scroll scroll-smooth hide-scrollbar"
                 >
                   <div
                     tabindex="0"
-                    v-for="(wallet, index) in slicedWallets"
-                    :key="wallet.Id"
+                    v-for="(wallet, index) in walletData"
+                    :key="index"
                     @click="selectWallet(wallet)"
-                    :ref="index === 0 ? 'firstWallet' : undefined"
                     :class="{
-                      'flex items-center px-4 py-2 focus:outline-none cursor-pointer hover:bg-gray-200 wallet-item': true,
-                      'bg-gray-200 wallet-item focus:outline-none': activeIndex === index,
+                      'flex justify-between items-center px-4 py-2 focus:outline-none cursor-pointer hover:bg-gray-200': true,
+                      'bg-gray-200 wallet-item focus:outline-none':
+                        getSelected === wallet.name || activeIndex === index,
                     }"
                   >
                     <!-- Wallet image with border-radius -->
+                    <div class="flex place-items-center gap-1">
+                      <img
+                        :src="wallet.image"
+                        alt=""
+                        class="w-7 h-7 mr-2 rounded-full object-cover bg-slate-300"
+                      />
+                      <span class="text-black">{{ wallet.name }}</span>
+                    </div>
                     <img
-                      :src="wallet.ImageUrl"
-                      alt="wallet image"
-                      class="w-7 h-7 mr-2 rounded-full object-cover"
+                      v-if="getSelected === wallet.name"
+                      src="/images/ok-2307342_640.png"
+                      alt="Wallet Image"
+                      class="w-5"
                     />
-                    <span class="text-black">{{ wallet.CoinName }}</span>
                   </div>
                 </div>
               </div>
